e8f8195ab664882f0a743ce5ccbc51669c3f82cb
z1mo0x <droweckiy@mail.ru>
2023-05-28 15:06:21 +0300

    23542345

--- js/template.js
+++ js/template.js
@@ -116,6 +116,8 @@ const exspos = [
 var selectedExspo = null;
 
 
+
+
 templateButtonElements.forEach((button, index) => {
 
     button.addEventListener('click', () => {
@@ -123,6 +125,7 @@ templateButtonElements.forEach((button, index) => {
         templateContent.style.maxHeight = 0;
         templateMainContainer.id = 'exspo' + (index + 1);
         selectedExspo = (index + 1);
+
         const editButton = document.getElementById('editButton');
         const exspoTitle1 = document.querySelector('.exspo1__block-title');
         const exspoDescr1 = document.querySelector('.exspo1__block-descr');
@@ -139,20 +142,24 @@ templateButtonElements.forEach((button, index) => {
         const exspoTitle3Input = document.getElementById('exspo3__block-title');
         const exspoDescr3Input = document.getElementById('exspo3__block-descr');
 
-
+        editButton.style.display = 'none';
 
         if (selectedExspo != null) {
+            editButton.style.display = 'block';
             if (selectedExspo == 1) {
                 editButton.addEventListener('click', () => {
+                    const title = document.querySelector('.title');
                     exspoDescr1.classList.toggle('hidden');
                     exspoTitle1.classList.toggle('hidden');
                     exspoTitle1Input.classList.toggle('hidden');
                     exspoDescr1Input.classList.toggle('hidden'); exspoTitle1.textContent = exspoTitle1Input.value;
                     exspoDescr1.textContent = exspoDescr1Input.value;
+                    title.textContent = exspoTitle1Input.value;
                 })
             }
             else if (selectedExspo == 2) {
                 editButton.addEventListener('click', () => {
+                    const title = document.querySelector('.title');
                     exspoDescr2.classList.toggle('hidden');
                     exspoTitle2.classList.toggle('hidden');
                     exspoDownText2.classList.toggle('hidden');
@@ -162,17 +169,19 @@ templateButtonElements.forEach((button, index) => {
                     exspoTitle2.textContent = exspoTitle2Input.value;
                     exspoDescr2.textContent = exspoDescr2Input.value;
                     exspoDownText2.textContent = exspoDownText2Input.value;
+                    title.textContent = exspoTitle2Input.value;
                 })
             } else if (selectedExspo == 3) {
                 editButton.addEventListener('click', () => {
                     exspoDescr3.classList.toggle('hidden');
+                    const titles = document.querySelector('.title');
                     exspoTitle3.classList.toggle('hidden');
                     exspoTitle3Input.classList.toggle('hidden');
                     exspoDescr3Input.classList.toggle('hidden'); exspoTitle3.textContent = exspoTitle3Input.value;
                     exspoDescr3.textContent = exspoDescr3Input.value;
+                    itle.textContent = exspoTitle3Input.value;
                 })
             }
-
         }
     })
 
