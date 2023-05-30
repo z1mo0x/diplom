069767853fd1460ae62aa479cfe6b143917e02a9
z1mo0x <droweckiy@mail.ru>
2023-05-31 00:01:49 +0300

    fffff

--- js/template.js
+++ js/template.js
@@ -1,126 +1,19 @@
 const templateMainContainer = document.getElementById('template');
 const templateButtonElements = document.querySelectorAll('.template__select > button');
-
-const exspos = [
-    `<div class="exspo1">
-    <div class="exspo1__title title">Название</div>
-    <div class="container exspo1__wrapper">
-        <div class="exspo1__block">
-            <img class="exspo1__bg" src="img/example2.webp" alt="">
-            <div class="exspo1__images">
-                <img src="img/exspo1.jpg" alt="">
-                <img src="img/exspo2.jpg" alt="">
-            </div>
-            <div class="exspo1__block-title hidden">
-                Электромеханическая счётно-табличная машина "АСКОТА"
-            </div>
-                <input type="text" placeholder='Введите название экспозиции' class="" id="exspo1__block-title"/>
-            <div class="exspo1__block-descr hidden">
-                Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и могла
-                удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в минуту.
-                Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и могла
-                удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в минуту.
-                Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и могла
-                удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в минуту.
-
-            </div>
-            <textarea placeholder='Введите информацию о экспозиции' cols="100" rows="10" class="" id="exspo1__block-descr"></textarea>
-            <div class="exspo1__block-link">
-                <a href="#">Перейти к главной странице</a>
-            </div>
-        </div>
-    </div>
-</div>`,
-    `<div class="exspo2">
-<div class="exspo2__title title">Название</div>
-<div class="container exspo2__wrapper">
-    <div class="exspo2__block">
-        <img class="exspo2__bg" src="img/example1.webp" alt="">
-        <div class="exspo2__top">
-            <div class="exspo2__images">
-                <img src="img/exspo1.jpg" alt="">
-            </div>
-            <div class="exspo2__text">
-                <div class="exspo2__block-title  hidden">
-                    Электромеханическая счётно-табличная машина "АСКОТА"
-                </div>
-                <input type="text" placeholder='Введите название экспозиции' class="" id="exspo2__block-title"/>
-                <div class="exspo2__block-descr  hidden">
-                    Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и
-                    могла
-                    удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в
-                    минуту. могла
-                    удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в
-                    минуту.
-                </div>
-                <textarea placeholder='Введите текст экспозиции' cols="40" rows="10" class="" id="exspo2__block-descr"></textarea>
-            </div>
-        </div>
-        <div class="exspo2__downtext hidden">
-            Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и могла
-            удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в минуту.
-            Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и могла
-            удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в минуту.
-            Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и могла
-            удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в минуту.
-            Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и могла
-            удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в минуту.
-        </div>
-        <textarea placeholder='Введите текст экспозиции' cols="100" rows="10" class="" id="exspo2__block-downtext"></textarea>
-        <div class="exspo1__block-link">
-            <a href="#">Перейти к главной странице</a>
-        </div>
-    </div>
-</div>
-</div>`, `<div class="exspo3">
-<div class="exspo3__title title">Название</div>
-<div class="container exspo3__wrapper">
-    <div class="exspo3__block">
-        <img class="exspo3__bg" src="img/example4.webp" alt="">
-        <div class="exspo3__images">
-            <img src="img/exspo1.jpg" alt="">
-            <img src="img/exspo2.jpg" alt="">
-
-        </div>
-        <div class="exspo3__down">
-            <div class="exspo3__logo">
-                <img src="img/exspo1.jpg" alt="">
-            </div>
-            <div class="exspo3__text">
-                <div class="exspo3__block-title hidden">
-                    Электромеханическая счётно-табличная машина "АСКОТА"
-                </div>
-                <input type="text" placeholder='Введите название экспозиции' id="exspo3__block-title"/>
-                <div class="exspo3__block-descr hidden">
-                    Она умела считать только на ленту. Она была плохо приспособлена для скоростного набора и
-                    могла
-                    удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в
-                    минуту. могла
-                    удовлетворять потребностям только юных бухгалтеров, работающих со скоростью 60 знаков в
-                    минуту.
-                </div>
-                <textarea placeholder='Введите текст экспозиции' cols="50" rows="10" class="" id="exspo3__block-descr"></textarea>
-            </div>
-        </div>
-        <div class="exspo3__block-link">
-            <a href="#">Перейти к главной странице</a>
-        </div>
-    </div>
-</div>
-</div>`];
-
+const hiddenElements = document.querySelectorAll('.hidden-exspo');
 var selectedExspo = null;
-
-
-
 
 templateButtonElements.forEach((button, index) => {
 
     button.addEventListener('click', () => {
-        templateMainContainer.innerHTML = exspos[index];
         templateContent.style.maxHeight = 0;
         templateMainContainer.id = 'exspo' + (index + 1);
         selectedExspo = (index + 1);
+        console.log(selectedExspo);
+        hiddenElements.forEach((element) => {
+            element.classList.add('hidden-exspo');
+        })
+        hiddenElements[selectedExspo - 1].classList.toggle('hidden-exspo');
 
         const editButton = document.getElementById('editButton');
         const exspoTitle1 = document.querySelector('.exspo1__block-title');
@@ -180,6 +73,5 @@ templateButtonElements.forEach((button, index) => {
             }
         }
     })
-
 });
 
